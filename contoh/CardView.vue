61aad21447cce6df259de05b35189c18d3cf726b
Shevy Nur Alviani <shevynur.alviani@gmail.com>
2024-03-18 22:59:57 +0700

    membuat struktur pimpinan view

---
+++ contoh/CardView.vue
@@ -0,0 +1,38 @@
+<template>
+    <!-- container nav Tentang -->
+    <div class="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+      <div class="border-b border-stroke py-20 px-20 dark:border-strokedark">
+        <div class="text-center text-black-900 font-bold text-title-xxl mb-4">Sambutan Ketua Cabang</div>     
+        <div class="max-w-sm w-full lg:max-w-full lg:flex">
+          <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/src/assets/images/ustad subki al buqhury.jpg" alt="">
+          <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            <div class="mb-8">            
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+              </svg>
+              <p class="mt-2 text-black text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+              <p class="mt-2 text-black text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam inventore impedit minus soluta ab quaerat quas unde porro accusantium magni consequuntur aperiam earum praesentium eius, cupiditate dolor quis dicta voluptatum?</p>
+              <p class="mt-2 text-black text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam inventore impedit minus soluta ab quaerat quas unde porro accusantium magni consequuntur aperiam earum praesentium eius, cupiditate dolor quis dicta voluptatum?</p>
+              <p class="mt-2 text-black text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam inventore impedit minus soluta ab quaerat quas unde porro accusantium magni consequuntur aperiam earum praesentium eius, cupiditate dolor quis dicta voluptatum?</p>
+              <p class="mt-2 text-black text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam inventore impedit minus soluta ab quaerat quas unde porro accusantium magni consequuntur aperiam earum praesentium eius, cupiditate dolor quis dicta voluptatum?</p>
+            </div>
+            <div class="flex items-center">
+              <img class="w-10 h-10 rounded-full mr-4" src="/src/assets/images/ustad subki al buqhury.jpg" alt="Avatar of Jonathan Reinink">
+              <div class="text-sm">
+                <p class="text-gray-900 leading-none">Subkhi Al-Bughury</p>
+                <p class="text-gray-600">Aug 18</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Card -->
+    <a href="#" class="flex flex-col items-center bg-white border border-gray-300 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+  <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/src/assets/images/ustad subki al buqhury.jpg" alt="">
+  <div class="flex flex-col justify-between p-4 leading-normal">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Sambutan Ketua Cabang</h5>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+  </div>
+</a>
+</template>
---
+++ contoh/StructurPimpinanView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="bg-white py-24 sm:py-32">
+    <div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+      <div class="max-w-2xl">
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">STRUKTUR PIMPINAN CABANG MUHAMMADIYAH (PCM) BATAM KOTA</h2>
+        <p class="mt-6 text-lg leading-8 text-gray-600">Libero fames augue nisl porttitor nisi, quis. Id ac elit odio vitae elementum enim vitae ullamcorper suspendisse.</p>
+      </div>
+      <ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+        <li v-for="person in people" :key="person.name">
+          <div class="flex items-center gap-x-6">
+            <img class="h-20 w-20 rounded-full" :src="person.imageUrl" alt="" />
+            <div>
+              <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">{{ person.name }}</h3>
+              <p class="text-sm font-semibold leading-6 text-indigo-600">{{ person.role }}</p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const people = [
+  {
+    name: 'Ustaz Subkhi Al-Bughury',
+    role: 'Ketua Pimpinan Cabang Batam Kota',
+    imageUrl:
+      'https://3.bp.blogspot.com/-J59lvVFtt0I/WSgTxM_upNI/AAAAAAAABuA/Gmljji2gw2kEYIXKuBAFnxit-DcU3NfaQCLcB/s1600/profil%2Bbiodata%2Bustadz%2Bsubki%2Bal%2Bbughury.jpg',
+  },
+  {
+  name: 'Ustaz Subkhi Al-Bughury',
+    role: 'Ketua Pimpinan Cabang Batam Kota',
+    imageUrl:
+      'https://3.bp.blogspot.com/-J59lvVFtt0I/WSgTxM_upNI/AAAAAAAABuA/Gmljji2gw2kEYIXKuBAFnxit-DcU3NfaQCLcB/s1600/profil%2Bbiodata%2Bustadz%2Bsubki%2Bal%2Bbughury.jpg',
+  },
+  {
+    name: 'Ustaz Subkhi Al-Bughury',
+    role: 'Ketua Pimpinan Cabang Batam Kota',
+    imageUrl:
+      'https://3.bp.blogspot.com/-J59lvVFtt0I/WSgTxM_upNI/AAAAAAAABuA/Gmljji2gw2kEYIXKuBAFnxit-DcU3NfaQCLcB/s1600/profil%2Bbiodata%2Bustadz%2Bsubki%2Bal%2Bbughury.jpg',
+  },
+]
+</script>
